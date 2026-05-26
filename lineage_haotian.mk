1d052cc65df2b3a3a2bca20ad0537a8ded476e6a
Rep1ace <yang_le_han@outlook.com>
2026-05-26 21:35:31 +0800

    haotian: update stock build fingerprint
    Change-Id: I04853c111f88cf41d818044d6ec2328e4c07cca5

--- lineage_haotian.mk
+++ lineage_haotian.mk
@@ -25,9 +25,9 @@ PRODUCT_MODEL := 2410DPN6CC
 PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc=$(call normalize-path-list, "haotian haotian 16 BP2A.250605.031.A3 OS3.0.5.0.WOBCNXM:user release-keys")
+    BuildDesc=$(call normalize-path-list, "haotian haotian 16 BP2A.250605.031.A3 OS3.0.302.0.WOBCNXM:user release-keys")
 
-BUILD_FINGERPRINT := Xiaomi/haotian/haotian:16/BP2A.250605.031.A3/OS3.0.5.0.WOBCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/haotian/haotian:16/BP2A.250605.031.A3/OS3.0.302.0.WOBCNXM:user/release-keys
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
