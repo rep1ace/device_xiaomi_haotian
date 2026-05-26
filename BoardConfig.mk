7cae3b7699f291d9c7999e3cd4169c5ea8e882ce
Rep1ace <yang_le_han@outlook.com>
2026-05-26 21:03:56 +0800

    haotian: point builds at prebuilt kernel headers
    Change-Id: Id5496d14042583a6887f0006e25175d8931bd588

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,10 +15,12 @@ TARGET_SCREEN_DENSITY := 520
 
 # Dtb/o
 BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
+# Soong's prebuilt DTB packaging only globs *.dtb from this directory.
 BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
 
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
+TARGET_PREBUILT_KERNEL_HEADERS := $(KERNEL_PATH)/prebuilt_kernel_headers.tar.gz
 PRODUCT_COPY_FILES += \
 	$(KERNEL_PATH)/kernel:kernel
 
@@ -29,8 +31,8 @@ BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_dl
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_dlkm/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) \
-    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
-    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/system_dlkm/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules/6.6.30-android15-8-geae86f166c48-abogki367569362-4k)
+    $(filter-out $(KERNEL_PATH)/vendor_ramdisk/modules.load.recovery:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules/modules.load.recovery,$(call find-copy-subdir-files,*,$(KERNEL_PATH)/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules)) \
+    $(call find-copy-subdir-files,*,$(KERNEL_PATH)/system_dlkm/6.6.77-android15-8-gca30f3b4bef6-abogki440974771-4k/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules/6.6.77-android15-8-gca30f3b4bef6-abogki440974771-4k)
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
